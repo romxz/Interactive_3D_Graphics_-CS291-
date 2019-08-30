54cf7c5423480e9a16d441ee18d4e8eda6f09e5b
andrei <andreiromalv@gmail.com>
2019-08-30 18:35:25 -0400

    completing db face exercise

--- unit5/unit5-db_geom_exercise.js
+++ unit5/unit5-db_geom_exercise.js
@@ -162,19 +162,44 @@ function createDrinkingBird() {
 	var eyeMaterial = new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0x303030, shininess: 4 } );
 
 	// YOUR CODE HERE
-	// Add a crossbar support, a nose, and eyes.
+	// Add a crossbar sp port, a nose, and eyes.
+
 	// Crossbar: use crossbarMaterial and
 	//   THREE.CylinderGeometry( XX, XX, XX, 32 ) for the tessellation.
 	//   The cylinder should have a radius of 5, length 200 and be at height Y=360
 	//   and rotated 90 degrees into position.
-	// Nose: use headMaterial and
+	cylinder = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 200, 32), crossbarMaterial);
+	cylinder.position.set(0, 360, 0);
+  	cylinder.rotation.x = 90*Math.PI/180.0;
+  	scene.add(cylinder);
+
+  	// Nose: use headMaterial and
 	//   THREE.CylinderGeometry( XX, XX, XX, 32 ) for the tessellation.
 	//   The cone should have a radius of 6 at the tip, 14 at the bottom, height 70
 	//   It should be rotated 90 degrees and put into position at -70, 530.
+  	cylinder = new THREE.Mesh(new THREE.CylinderGeometry(6, 14, 70, 32), headMaterial);
+  	cylinder.position.set(-70, 530, 0);
+  	cylinder.rotation.z = 90*Math.PI/180.0;
+  	scene.add(cylinder);
+
 	// Eyes: use eyeMaterial and
 	//   THREE.SphereGeometry( XX, 32, 16 ) for the tessellation.
 	//   Each sphere should have radius of 10 and be moved to X=-48, Y=560,
 	//   then rotated 20 degrees left and right to make a pair.
+  	// Right eye
+  	var eye = new THREE.Object3D();
+  	sphere = new THREE.Mesh(new THREE.SphereGeometry(10, 32, 16), eyeMaterial);
+  	sphere.position.set(-48,560,0);
+  	eye.add(sphere);
+  	eye.rotation.y = 20*Math.PI/180;
+  	scene.add(eye);
+  	// Left eye
+  	eye = new THREE.Object3D();
+  	sphere = new THREE.Mesh(new THREE.SphereGeometry(10, 32, 16), eyeMaterial);
+  	sphere.position.set(-48,560,0);
+  	eye.add(sphere);
+  	eye.rotation.y = -20*Math.PI/180;
+  	scene.add(eye);
 }
 
 function fillScene() {
